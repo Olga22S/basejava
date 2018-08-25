8f09adbe0da2fbb777d57b65a05072f3a21e9acc
Olga Pyshkina <ospyshkina@yandex.ru>
2018-08-25 23:19:31 +0300

    Project is done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,67 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
-    void save(Resume r) {
+    void save(Resume r)  {
+
+        for(int i=0;i<storage.length;i++) {
+            if(storage[i]==null) {
+                storage[i]=r;
+                break;}
+            else if(storage[i]!=null){
+                if(storage[i].uuid.contains(r.uuid)) {
+                    System.out.println("This resume is already exist");
+                    break ;
+                }
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume r = new Resume();
+        r.uuid = "not found";
+        for(int j =0;j<storage.length;j++)
+            if(storage[j]!=null)
+                if(storage[j].uuid.contains(uuid))
+                    r.uuid = uuid;
+        return r;
     }
 
     void delete(String uuid) {
+        for(int j =0;j<storage.length;j++){
+            if(storage[j]!=null) {
+                if(storage[j].uuid.contains(uuid)){
+                    storage[j]=null;
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        Resume[] resume = new Resume[size()];
+
+        for(int j=0;j<size();j++)
+            for(int i =0;i<storage.length;i++)
+                if((storage[i]!=null)) {
+                    resume[j]=storage[i];
+                    j++;
+                    continue;
+                }else continue;
+        return resume;
     }
 
     int size() {
-        return 0;
+        int size=0;
+        for(int i =0;i<storage.length;i++) {
+            if(!(storage[i]==null))
+                size++;
+        }
+        return size;
     }
 }
