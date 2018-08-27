790095d41a7a67c5c38dc239540081fa0e7ac1da
Olga Pyshkina <ospyshkina@yandex.ru>
2018-08-27 22:10:58 +0300

    Corrected

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,39 +5,38 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-private int size;
+    private int size;
 
     void clear() {
-        int i=0;
-        Arrays.fill(storage, i, size(), null);
+        Arrays.fill(storage, 0, size(), null);
         size = 0;
     }
 
-    void save(Resume r)  {
-        storage[size]=r;
+    void save(Resume r) {
+        storage[size] = r;
         size++;
     }
 
     Resume get(String uuid) {
-        Resume r = new Resume();
-        for(int j =0;j<storage.length;j++) {
-            if (storage[j] != null) {
-                if (storage[j].uuid.contains(uuid)) {
-                    r.uuid = uuid;
-                }
+        for (int j = 0; j < size; j++) {
+            if (storage[j].uuid.contains(uuid)) {
+                return storage[j];
             }
         }
-        return r;
+        return null;
     }
 
     void delete(String uuid) {
-        for(int j =0;j<storage.length;j++){
-            if(storage[j]!=null) {
-                if(storage[j].uuid.contains(uuid)){
-                    storage[j]=null;
+        for (int j = 0; j < size(); j++) {
+
+            if (storage[j].uuid.contains(uuid)) {
+                storage[j] = null;
+                for (int i = j; j < size(); j++) {
+                    storage[j] = storage[j + 1];
                 }
             }
         }
+
         size--;
     }
 
@@ -45,21 +44,7 @@ private int size;
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] resume = new Resume[size];
-
-        for(int i =0;i<storage.length;i++) {
-            if(storage[i]==null) {
-                for(int j=i+1;j<storage.length;j++) {
-                    if(storage[j]!=null) {
-                        Resume m = storage[i];
-                        storage[i]=storage[j];
-                        storage[j]=m;;
-                        break;
-                    }
-                }
-            }
-        }
-        resume = Arrays.copyOf(storage, size);
+        Resume[] resume = Arrays.copyOf(storage, size);
         return resume;
     }
 
